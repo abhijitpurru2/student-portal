8a0541050f14cf1a2d5b58bfaee1000717cf2dcb
abhijitpurru2 <abhijitpurru@gmail.com>
2024-04-03 20:57:25 -0400

    Added SQL files

--- src/main/java/com/student/studentportal/StudentPortalApplication.java
+++ src/main/java/com/student/studentportal/StudentPortalApplication.java
@@ -8,6 +8,7 @@ public class StudentPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentPortalApplication.class, args);
+		System.out.println("Connected!");
 	}
 
 }
