aaf4f7577107bce83e3fa1fed0bb9044387845d9
abhijitpurru2 <abhijitpurru@gmail.com>
2024-05-03 22:06:28 -0400

    Added Security Config

--- rest-api/src/main/java/com/project/RestAPIApp.java
+++ rest-api/src/main/java/com/project/RestAPIApp.java
@@ -8,12 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
+public class RestAPIApp
 {
 
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(RestAPIApp.class, args);
         System.out.println( "Hello World!" );
     }
 }
